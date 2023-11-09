114fdfe2a4a82dea18e833292967e6b20da44983
Vinícius Costa <vini.pc03@gmail.com>
2023-11-09 13:58:29 -0300

    criando, players e movimentação

--- index.js
+++ index.js
@@ -4,23 +4,30 @@ const c = canvas.getContext('2d');
 canvas.width = 1024
 canvas.height = 576
 
-c.fillRect(0, 0, canvas.width, canvas.height);
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const gravity = 0.2
 
 class Sprite {
     constructor({position, velocity}){
         this.position = position
         this.velocity = velocity
+        this.height =150
     }
 
     draw(){
         c.fillStyle = 'red'
-    c.fillRect(this.position.x, this.position.y, 50, 150);
+    c.fillRect(this.position.x, this.position.y, 50, this.height);
     }
 
     update(){
-        this.draw(
-        this.position.y = this.position.y += 10
-        )
+        this.draw()
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y = 0;
+        }else this.velocity.y += gravity
     }
 }
 
@@ -48,12 +55,66 @@ const enemy = new Sprite({
 
 console.log(player);
 
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    w: {
+        pressed: false
+    }
+}
+let lastKey
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
+
+    player.velocity.x = 0
+
+    if (keys.a.pressed && lastKey === 'a') {
+        player.velocity.x = -1
+    }else if(keys.d.pressed && lastKey === 'd'){
+        player.velocity.x = 1
+    }
 }
 
 animate()
+
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+            break
+        case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
+            break
+        case 'w':
+            player.velocity.y = -10
+            break
+    }
+    console.log(event.key);
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = false
+        break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 'w':
+            keys.w.pressed = false
+            break
+    }
+    console.log(event.key);
+})
